09f2c20f1b925a39b651aba859bd302730475c79
AD <atar.dci@gmail.com>
2019-06-12 15:38:24 +0200

    Wrap App in Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.querySelector("#root"));
 
 serviceWorker.unregister();
