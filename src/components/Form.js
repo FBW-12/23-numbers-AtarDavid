ff09e3a89145433081d4eed4a3450a58febc0459
AD <atar.dci@gmail.com>
2019-06-12 15:40:29 +0200

    Integrate redux, update callback function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Form extends Component {
+class Form extends Component {
     render() {
+        const { onRequestNumber } = this.props;
         return (
             <div>
                 <label htmlFor="number">Insert a number</label>
                 <br />
                 <input name="number" type="number" defaultValue="23" />
                 <br />
-                <button onClick={() => { console.log("Do something") }}>Show info about this number</button>
+                <button onClick={onRequestNumber}>Show info about this number</button>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return state
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onRequestNumber: () => dispatch({ type: "REQUEST" })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
